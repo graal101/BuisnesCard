4d1a229a26958c819581a9e96fa204e2307d28b9
Andrew Andrey <graal101@mail.ru>
2025-07-18 16:11:26 +0700

    real ip in db

--- main.py
+++ main.py
@@ -2,19 +2,27 @@
 # -*- coding: utf-8 -*-
 """Business card website for study and practice."""
 from datetime import datetime
-from tools.table import Tables
-import pytz
+
 from flask import Flask, render_template, request
 
+import pytz
+
+from tools.table import Tables
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/index')
 def home():
     """Home page."""
-    db = Tables()
-    ip_visitor = request.remote_addr
-    tmstr = datetime.now(tz=pytz.timezone('Europe/Moscow')).strftime('%Y-%m-%d(%H:%M:%S)') 
+    db = Tables()  # FIX with alchimy!!!
+    ip_visitor = request.headers.get('X-Real-IP')
+    # ip_visitor = request.remote_addr
+    # ip_visitor = request.headers.get('X-Forwarded-For')
+    # user_agent = request.user_agent.string
+    tmstr = datetime.now(tz=pytz.timezone('Europe/Moscow')).strftime('%Y-%m-%d(%H:%M:%S)')
     db.add_info(ip_visitor, tmstr)
     return render_template('index.html')
 
