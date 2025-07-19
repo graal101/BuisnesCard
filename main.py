ab810694a71b190dff8345879da3941ed0384a88
Andrew Andrey <graal101@mail.ru>
2025-07-19 20:34:26 +0700

    did with SQLAlchemy

--- main.py
+++ main.py
@@ -9,22 +9,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 import pytz
 
-from tools.table import Tables
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///visitors.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    """Reg of unique customers."""   
+    ids = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ip = db.Column(db.String(20), unique=False, nullable=True)
+    visittime = db.Column(db.String(25), unique=False, nullable=True)
+    user_agent = db.Column(db.Text, unique=False, nullable=True)
+
 
 
 @app.route('/')
 @app.route('/index')
 def home():
     """Home page."""
-    db = Tables()  # FIX with alchimy!!!
     ip_visitor = request.headers.get('X-Real-IP')
     user_agent = request.user_agent.string
     tmstr = datetime.now(tz=pytz.timezone('Europe/Moscow')).strftime('%Y-%m-%d(%H:%M:%S)')
-    db.add_info(ip_visitor, tmstr, user_agent)
+    with app.app_context():
+        db.create_all()
+        new_entry = Users(ip=ip_visitor, visittime=tmstr, user_agent=user_agent)
+        db.session.add(new_entry)
+        db.session.commit()
     return render_template('index.html')
 
 
