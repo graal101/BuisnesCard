b9c19b91f090f211f6257600e8fcd591d3fb8fbe
Andrew Andrey <graal101@mail.ru>
2025-07-24 18:55:34 +0700

    Transition to SQLAlchemy

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Business card website for study and practice."""
-from datetime import datetime
-
 from flask import Flask, render_template, request
 
-from flask_sqlalchemy import SQLAlchemy   # FIX with models.py
-
-import pytz  # FIX with SQLAlchemy
+from models import Guests, db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///visitors.db'  # FIX with models.py
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///visitors.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning
-
-db = SQLAlchemy(app)
-
-
-class Guests(db.Model):
-    """Reg of unique customers."""
-
-    ids = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    ip = db.Column(db.String(20), unique=False, nullable=True)
-    visittime = db.Column(db.String(25), unique=False, nullable=True)  # FIX with SQLAlchemy
-    user_agent = db.Column(db.Text, unique=False, nullable=True)
+db.init_app(app)
 
 
 @app.route('/')
@@ -33,7 +19,6 @@ def home():
         db.create_all()
         new_entry = Guests(
             ip=request.headers.get('X-Real-IP'),
-            visittime=datetime.now(tz=pytz.timezone('Europe/Moscow')).strftime('%Y-%m-%d(%H:%M:%S)'),
             user_agent=request.user_agent.string
         )
         db.session.add(new_entry)
@@ -45,7 +30,7 @@ def home():
 def print_sqlite():
     """Print visitors, count."""
     res = Guests.query.all()
-    lst: list = [[person.ip, person.visittime, person.user_agent] for person in res]
+    lst: list = [[person.ip, person.user_agent, person.visittime] for person in res]
     return render_template('print.html', vst=lst)
 
 
--- models.py
+++ models.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Model."""
 from flask_sqlalchemy import SQLAlchemy
 
+from sqlalchemy import text
+
+db = SQLAlchemy()
 
 
+class Guests(db.Model):
+    """Reg of unique customers."""
 
+    __tablename__ = 'guests'
+
+    ids = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ip = db.Column(db.String(20), unique=False, nullable=True)
+    user_agent = db.Column(db.Text, unique=False, nullable=True)
+    visittime = db.Column(db.DateTime, server_default=text("DATETIME('now', '+3 hours')"))  # SQL method (Europe/Moscow)
+    #  visittime = db.Column(DateTime, server_default=func.now()) Native method
