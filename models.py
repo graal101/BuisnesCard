0db35a4c7ae137d32199503cde11c73b3d77336e
Andrew Andrey <graal101@mail.ru>
2025-07-25 18:05:44 +0700

    "After practice."

--- models.py
+++ models.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 """Model."""
 from flask_sqlalchemy import SQLAlchemy
-
-from sqlalchemy import text
+from sqlalchemy.sql import func
+from sqlalchemy import  DateTime
 
 db = SQLAlchemy()
 
@@ -11,10 +11,9 @@ db = SQLAlchemy()
 class Guests(db.Model):
     """Reg of unique customers."""
 
-    __tablename__ = 'guests'
 
     ids = db.Column(db.Integer, primary_key=True, autoincrement=True)
     ip = db.Column(db.String(20), unique=False, nullable=True)
     user_agent = db.Column(db.Text, unique=False, nullable=True)
-    visittime = db.Column(db.DateTime, server_default=text("DATETIME('now', '+3 hours')"))  # SQL method (Europe/Moscow)
-    #  visittime = db.Column(DateTime, server_default=func.now()) Native method
+    visittime = db.Column(db.DateTime, server_default=func.now())
+
